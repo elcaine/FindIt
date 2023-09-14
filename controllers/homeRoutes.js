1a977d6af5eec8c9715375caab74d8e3a3f06017
Caine Winters@meNZ <caine.winters@outlook.com>
2023-09-14 11:07:14 -0400

    New model: Category.  Modified seed execution file and built seed data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,11 +14,20 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Categories list
+router.get('/categories', async (req, res) => {
+  try{
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+
 // Login page
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/profile'); // TODO:  Not sure if this route is setup yet
     return;
   }
 
